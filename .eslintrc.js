108ee40fc8d0fe32ddc4f5bf889fd7cf283d0ace
JTPeng <404502753@qq.com>
2022-09-22 22:33:19 +0800

    rollup 添加eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    // "es2021": true
+  },
+  // "extends": ["plugin:vue/essential", "eslint:recommended"],
+  extends: "plugin:vue/essential",
+  parserOptions: {
+    ecmaVersion: 10,
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {},
+};
