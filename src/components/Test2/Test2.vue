e41f739e4b2e848c84d8437f40bffeb42f0592b6
JTPeng <404502753@qq.com>
2022-10-08 22:20:39 +0800

    yarn link与svg练习

---
+++ src/components/Test2/Test2.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="test-svg">
+    <svg width="200px" height="200px" viewBox="0 0 100 100">
+      <circle r="5" cx="10" cy="25" fill="currentColor" />
+      <circle r="5" cx="10" cy="50" fill="currentColor" />
+      <circle r="5" cx="10" cy="75" fill="currentColor" />
+      <line
+        x1="40"
+        y1="25"
+        x2="90"
+        y2="25"
+        stroke-width="8"
+        stroke="currentColor"
+      />
+      <line
+        x1="40"
+        y1="50"
+        x2="80"
+        y2="50"
+        stroke-width="8"
+        stroke="currentColor"
+      />
+      <line
+        x1="40"
+        y1="75"
+        x2="90"
+        y2="75"
+        stroke-width="8"
+        stroke="currentColor"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test1",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.test {
+  color: red;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Test from "./components/Test/index";
+import Test2 from "./components/Test2/index";
 export default function (Vue) {
   Vue.use(Test);
+  Vue.use(Test2)
 }
