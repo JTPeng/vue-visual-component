9f9b0ee6619fe1cfe3f151551cef0cc718b6a33d
JTPeng <404502753@qq.com>
2022-10-18 20:29:32 +0800

    svg基本使用

--- src/components/Test2/Test2.vue
+++ src/components/Test2/Test2.vue
@@ -1,33 +1,54 @@
 <template>
   <div class="test-svg">
-    <svg width="200px" height="200px" viewBox="0 0 100 100">
-      <circle r="5" cx="10" cy="25" fill="currentColor" />
-      <circle r="5" cx="10" cy="50" fill="currentColor" />
-      <circle r="5" cx="10" cy="75" fill="currentColor" />
-      <line
-        x1="40"
-        y1="25"
-        x2="90"
-        y2="25"
-        stroke-width="8"
-        stroke="currentColor"
-      />
-      <line
-        x1="40"
-        y1="50"
-        x2="80"
-        y2="50"
-        stroke-width="8"
-        stroke="currentColor"
-      />
-      <line
-        x1="40"
-        y1="75"
-        x2="90"
-        y2="75"
-        stroke-width="8"
-        stroke="currentColor"
-      />
+    <svg width="0" height="0">
+      <defs>
+        <symbol id="more" viewBox="0 0 100 100">
+          <circle r="5" cx="10" cy="25" fill="currentColor" />
+          <circle r="5" cx="10" cy="50" fill="currentColor" />
+          <circle r="5" cx="10" cy="75" fill="currentColor" />
+          <line
+            x1="40"
+            y1="25"
+            x2="90"
+            y2="25"
+            stroke-width="8"
+            stroke="currentColor"
+          />
+          <line
+            x1="40"
+            y1="50"
+            x2="80"
+            y2="50"
+            stroke-width="8"
+            stroke="currentColor"
+          />
+          <line
+            x1="40"
+            y1="75"
+            x2="90"
+            y2="75"
+            stroke-width="8"
+            stroke="currentColor"
+          />
+        </symbol>
+        <symbol id="filledArrowRight" viewBox="0 0 100 100">
+          <polyline points="20 10, 80 50, 20 90" fill="currentColor"></polyline>
+        </symbol>
+        <symbol id="arrowRight" viewBox="0 0 100 100">
+          <polyline
+            points="30 20, 70 50, 30 80"
+            fill="transparent"
+            stroke="currentColor"
+          ></polyline>
+        </symbol>
+        <symbol id="selected" viewBox="0 0 1024 1024">
+          <path
+            fill="currentColor"
+            d="M464.247574 677.487844C474.214622 686.649009 489.665824 686.201589 499.086059 676.479029L798.905035 367.037898C808.503379 357.131511 808.253662 341.319801 798.347275 331.721456 788.44089 322.12311 772.62918 322.372828 763.030833 332.279215L463.211858 641.720346 498.050342 640.711531 316.608839 473.940462C306.453341 464.606085 290.653676 465.271736 281.319299 475.427234 271.984922 485.582733 272.650573 501.382398 282.806071 510.716775L464.247574 677.487844Z"
+            p-id="2904"
+          ></path>
+        </symbol>
+      </defs>
     </svg>
   </div>
 </template>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import Test from "./components/Test/index";
 import Test2 from "./components/Test2/index";
+import Icon from "./components/Icon/index";
+import SvgAnimation from "./components/SvgAnimation/index";
 export default function (Vue) {
+  // 组件注册
   Vue.use(Test);
-  Vue.use(Test2)
+  Vue.use(Test2);
+  Vue.use(Icon);
+  Vue.use(SvgAnimation);
 }
